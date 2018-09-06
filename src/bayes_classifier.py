00e6e0a64638dee62fe6cc821bf3deb739dbf96c
Manas Joshi <manasjoshi241@gmail.com>
2018-09-07 04:24:22 +0530

    Structured Code

--- src/bayes_classifier.py
+++ src/bayes_classifier.py
@@ -3,20 +3,29 @@ import class_conditional_estimator as cc
 
 class bayes_model:
 
+	# Initialization
 	def __init__(self, estimator_type, param):
-		self.estimator = estimator_type
 		self.X = None 
 		self.Y = None
+
+		# Type of class conditional estimator to be used
+		self.estimator = estimator_type
+		# Parameters corresponding to the class conditional estimator
 		self.parameters = param
+		# List of class conditional estimator model objects per class
 		self.per_class_estimator = []
+		# List of class priors
 		self.prior = []
 
+	# Input m examples each of dimension d with corresponding labels
+	# X - [m x dim], Y - [m x 1]
 	def train(self, X_train, Y_train):
 		self.X = X_train
 		self.Y = Y_train
 
 		self._estimate_px_theta()
 
+	# Returns a model object for the class conditional estimator
 	def _get_est_object(self):
 		if self.estimator == 'GMM':
 			comp = self.parameters[0]
@@ -26,6 +35,7 @@ class bayes_model:
 			reg = self.parameters[4]
 			return cc.gaussian_mixture_model(n_comp = comp, tol = toler, max_iter = max_it, dimension = dim, regularize = reg)
 
+		# Implement other estimators
 		if self.estimator == '':
 			return None
 
@@ -35,6 +45,7 @@ class bayes_model:
 		if self.estimator == '':
 			return None
 
+	# Estimate parameters for class conditional estimators	
 	def _estimate_px_theta(self):
 		classes = np.unique(self.Y)
 		for i in classes:
@@ -46,10 +57,10 @@ class bayes_model:
 
 			(self.prior).append(np.sum(self.Y == i) / self.Y.shape[0])
 
+	# Returns class label for given m test examples each of dimension dim, X_test = [m x dim]
 	def predict(self, X_test):
 		classes = np.expand_dims(np.unique(self.Y), axis = 1)
 		Q = np.zeros((X_test.shape[0], classes.shape[0]))
-		Q
 		for i in range(len(classes)):
 			Q[:, i] = ((self.per_class_estimator)[i].get_likelihood(X_test) * (self.prior)[i]).ravel()
 		class_idx = np.argmax(Q, axis = 1)
