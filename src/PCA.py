7f2877abeed5ec7850e5b74b88e9185ff324972d
Shubham Singla <25100284+shubh1612@users.noreply.github.com>
2018-09-11 00:32:40 +0530

    Updated PCA - Unchecked

--- src/PCA.py
+++ src/PCA.py
@@ -6,23 +6,28 @@ class PCA:
     def _init_(self, n_components):
         # number of components to be projected upon
         self.n_components = n_components
+        # U matrix obtained after SVD of X
+        self.U = []
+        # mean of input data
+        self.mean = []
+        # S.D. of input data
+        self.sigma = []
 
     # Calculate mean
     def _mean(self):
         [m, n] = np.shape(self.X)
-        return (np.sum(self.X, axis = 0))/m
+        self.mean = (np.sum(self.X, axis = 0))/m
 
     # Calculate standard deviation
     def _sigma(self):
         variance = np.var(self.X, axis = 0)
-        sigma = np.sqrt(variance)
-        return sigma
-
+        self.sigma = np.sqrt(variance)
+        
     # Normalize data (Centering by subtracting mean)
     def _normalize(self):
         [m, n] = np.shape(self.X)
-        mu = self._mean()
-        X_norm = (self.X - mu)
+        self._mean()
+        X_norm = (self.X - self.mean)
         return X_norm
     
     # Calculate covariance matrix
@@ -35,15 +40,19 @@ class PCA:
         return np.linalg.svd(X_norm)
     
     # Projecting input data to smaller dimension
-    def _projectData(self, X_norm, U):
+    def _projectData(self, X_norm):
         k = self.n_components
-        return np.matmul(X_norm, U[:, 1:k+1])
+        return np.matmul(X_norm, self.U[:, 0:k])
         
-    # Function to be called for dimensionality reduction
-    def reduction(self, X_train, n_components):
+    # Function to be called for calculating U and mean
+    def fit(self, X_train, n_components):
         self.X = X_train
         self.n_components = n_components
         X_norm = self._normalize()
         [U, S, V] = self._svd(X_norm)
-        X_project = self._projectData(X_norm, U)
-        return X_project
+        self.U = U
+
+    # Function to give projected data 
+    def transform(self, X_test):
+        X_norm = X_test - self.mean
+        return self._projectData(X_norm)
