492b070645457f8943333d32bbae0610e666f140
Shubham Singla <25100284+shubh1612@users.noreply.github.com>
2018-09-12 01:15:16 +0530

    Updated Naive_Bayes Classifier. Needs to be verified once Class conditional densities are completed.

--- src/naive_bayes_classifier.py
+++ src/naive_bayes_classifier.py
@@ -4,18 +4,18 @@ import class_conditional_estimator as cc
 class naive_bayes_classifier:
 
 	# Initialization
-	def __init__(self, estimator_type, param):
-	self.X = None 
-	self.Y = None
+	def __init__(self, param, distrib):
+		self.X = None
+		self.Y = None
 
-	# Type of class conditional estimator to be used
-	self.estimator = estimator_type
-	# Parameters corresponding to the class conditional estimator
-	self.parameters = param
-	# List of class conditional estimator model objects per class
-	self.per_class_estimator = []
-	# List of class priors
-	self.prior = []
+		# Parameters corresponding to the class conditional estimator
+		self.parameters = param
+		# List of class conditional distributions for each feature
+		self.distributions = distrib
+		# List of class conditional estimator model objects per class
+		self.per_class_estimator = []
+		# List of class priors
+		self.prior = []
 
 	# Input m examples each of dimension d with corresponding labels
 	# X - [m x dim], Y - [m x 1]
@@ -26,64 +26,72 @@ class naive_bayes_classifier:
 		self._estimate_px_theta()
 
 	# Returns a model object for the class conditional estimator
-	def _get_est_object(self):
-		if self.estimator == 'GMM':
-			comp = self.parameters[0]
-			toler = self.parameters[1]
-			max_it = self.parameters[2]
-			dim = self.parameters[3]
-			reg = self.parameters[4]
+	def _get_est_object(self, feature_number):
+		if self.distributions[feature_number] == 'GMM':
+			comp = self.parameters[feature_number][0]
+			toler = self.parameters[feature_number][1]
+			max_it = self.parameters[feature_number][2]
+			dim = self.parameters[feature_number][3]
+			reg = self.parameters[feature_number][4]
 			return cc.gaussian_mixture_model(n_comp = comp, tol = toler, max_iter = max_it, dimension = dim, regularize = reg)
 
 		# Implement other estimators
-		if self.estimator == '':
+		if self.distributions[feature_number] == '':
 			return None
 
-		if self.estimator == '':
+		if self.distributions[feature_number] == '':
 			return None
 
-		if self.estimator == '':
+		if self.distributions[feature_number] == '':
 			return None
 
 	# Estimate parameters for class conditional estimators	
 	def _estimate_px_theta(self):
 		classes = np.unique(self.Y)
 		for i in classes:
-			print(i)
 			X_temp = (self.X)[np.repeat(self.Y == i, (self.X).shape[1], axis = 1)]
 			X_temp = X_temp.reshape(int(X_temp.shape[0] / (self.X).shape[1]), (self.X).shape[1] )
 
+			# Calculate model parameters for each feature vector
 			for j in range((self.X).shape[1]):
-				model = self._get_est_object()
+				model = self._get_est_object(j)
 				model.estimate_parameters(X_temp[:, j])
 				(self.per_class_estimator).append(model)
 
 			(self.prior).append(np.sum(self.Y == i) / self.Y.shape[0])
 
+		# 2D Matrix of weights for each class and feature distribution
 		self.per_class_estimator = (self.per_class_estimator).reshape(classes, (self.X).shape[1])
+
 
 	# Returns class label for given m test examples each of dimension dim, X_test = [m x dim]
 	def predict(self, X_test):
 		classes = np.expand_dims(np.unique(self.Y), axis = 1)
 		Q = np.zeros((X_test.shape[0], classes.shape[0]))
-		for i in range(len(classes)):
-			
-#			Q[:, i] = ((self.per_class_estimator)[i].get_likelihood(X_test) * (self.prior)[i]).ravel()
+
+		# Number of features
+		n = X_test.shape[1]
+		for i in range(classes):
+			for j in range(n):
+				# Calculate log of probability so that these can be added later since multiplication of probabilities will reduce to very small value
+				Q[:, i] = Q[:, i] + np.log(((self.per_class_estimator)[i][j].get_likelihood(X_test[:, j]) * (self.prior)[i]).ravel())
 		class_idx = np.argmax(Q, axis = 1)
 
 		return classes[class_idx]
 
 
 if __name__ == "__main__":
-	num_ex = 1000
+	num_ex = 100
 	dim = 5
 	X_train = (1000*np.random.rand(num_ex, dim)).astype('int')
 	Y_train = (5*np.random.rand(num_ex, 1)).astype('int')
 
-	parameters = [2, 0.001, 20, X_train.shape[1], 0.0001]
+	parameters = [1, 0.001, 20, 1, 0.0001]*5
+	distributions = ['GMM', 'GMM', 'GMM', 'GMM', 'GMM']
 
-	model1 = bayes_classifier(estimator_type = 'GMM', param = parameters)
+	model1 = naive_bayes_classifier(param = parameters, distrib = distributions)
 	model1.train(X_train, Y_train)
 
-	X_test = (1000*np.random.rand(10, dim)).astype('int')
+	print("Train complete")
+	X_test = (10*np.random.rand(10, dim)).astype('int')
 	print(model1.predict(X_test))
