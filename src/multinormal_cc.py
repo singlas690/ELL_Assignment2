85d1f111b721eeba4d193f47b74decd765bc6526
HarshShillak <kumar.harsh3682@gmail.com>
2018-09-12 13:44:16 +0530

    Tested for univariate case

--- src/multinormal_cc.py
+++ src/multinormal_cc.py
@@ -17,13 +17,15 @@ class multinormal_model:
         
         
     def estimate_parameters(self, X_train):
+        '''ONly pass a 2d np array, if column vector reshape(n_rows,1)'''
+        
         self.n_variables= (X_train).shape[1]
         mu = np.divide(X_train.sum(axis=0),(X_train).shape[0])
         self.mean_vector = mu
         sum_matrix = self.covariance
         for i in range(0,(X_train).shape[0]):
             X_i = X_train[i,:]
-            a=np.tensordot(np.subtract(X_i-self.mean_vector),np.subtract(X_i-self.mean_vector),axes=0)
+            a=np.tensordot((X_i-self.mean_vector),(X_i-self.mean_vector),axes=0)
             sum_matrix = np.add(a,sum_matrix)
         self.covariance= np.divide(sum_matrix, (X_train).shape[0])
         return [self.mean_vector, self.covariance]
@@ -35,3 +37,15 @@ class multinormal_model:
         p = (1/np.power(2*np.pi, self.n_variables/2))* (1/np.sqrt(np.abs(np.linalg.det(self.covariance))))* np.exp(exp_term)
         return np.array([p]).T
         
+if __name__ == "__main__":
+    
+    print("testing multinormal model:")
+    
+    X = np.array([np.random.normal(0,1,4000)]).T
+    model = multinormal_model()
+    parameters=model.estimate_parameters(X)
+    print("the estimated parameters by mle:\n")
+    print("mean:",parameters[0])
+    print("variance:",parameters[1])
+    from matplotlib import pyplot as plt
+    plt.scatter(X,model.get_likelihood(X))                                    
