90e9655aed5eebb7f7933de59da1129e864f75d8
Shubham Singla <25100284+shubh1612@users.noreply.github.com>
2018-10-13 12:08:03 +0530

    Correct FLDA

--- src/flda.py
+++ src/flda.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class flda:
 
@@ -6,7 +7,7 @@ class flda:
 		self.num_classes = num_classes
 		self.X = None
 		self.Y = None
-		self.weight = None
+		self.weight = []
 
 	def _find_mean(self, X_train, Y_train, k):
 		n, m = X_train.shape
@@ -14,23 +15,25 @@ class flda:
 
 		for i in range(n):
 			if (Y_train[i]==k):
-				mean1 = mean1 + X_train(i)
+				mean1 = mean1 + X_train[i]
+				n_prime += 1
 			else:
-				mean0 = mean0 + x_train(i)
+				mean0 = mean0 + X_train[i]
 
 		mean1 = mean1/n_prime
 		mean0 = mean0/(n-n_prime)
+		mean1 = mean1.reshape(1, m)
+		mean0 = mean0.reshape(1, m)
 		return [mean0, mean1]
 
 	def _find_sw(self, X_train, Y_train, mean0, mean1, k):
 		n, m = X_train.shape
 		sw = 0
-
 		for i in range(n):
 			if (Y_train[i]==k):
-				sw = sw + (X_train(i) - mean1)*(X_train(i) - mean1).T
+				sw = sw + np.matmul((X_train[i].reshape(1, m) - mean1).T, (X_train[i].reshape(1, m) - mean1))
 			else:
-				sw = sw + (X_train(i) - mean0)*(X_train(i) - mean0).T
+				sw = sw + np.matmul((X_train[i].reshape(1, m) - mean0).T, (X_train[i].reshape(1, m) - mean0))
 		return sw
 	
 	def train(self, X_train, Y_train):
@@ -38,21 +41,24 @@ class flda:
 		self.Y = Y_train
 
 		for k in range(self.num_classes):
-			Y_prime = [Y_train[labels==k] for labels in range(Y_train.shape[0])]
-			[mean0, mean1] = self._find_mean(X_train, Y_train, k)
-			sw = self._find_sw(X_train, Y_train, mean0, mean1, k)
-
-		wgt = np.matmul(np.linalg.inv(sw), (mean1 - mean0))
-		self.weight.append(wgt)
+			total = np.count_nonzero(Y_train == k)
+			if(total == 0):
+				wgt = np.zeros((X_train.shape[1], 1))
+			else:
+				[mean0, mean1] = self._find_mean(X_train, Y_train, k)
+				sw = self._find_sw(X_train, Y_train, mean0, mean1, k)
+				wgt = np.matmul(np.linalg.inv(sw), (mean1 - mean0).T)
+			self.weight.append(wgt)
 
 	def predict(self, X_test):
+		result = np.zeros([len(X_test),1])
 		Y_test = np.zeros([len(X_test),1])
 		for c in range(len(X_test)):
 			maxy = -1000000000000
 			for k in range(self.num_classes): 
-				if np.dot(X_test[c],weight[k]) > maxy:
+				if np.dot(X_test[c], self.weight[k]) > maxy:
 					class_pred = k 
-					maxy = np.dot(X_test[c],weights[k])
+					maxy = np.dot(X_test[c], self.weight[k])
 			result[c] = class_pred
 		return result
 
